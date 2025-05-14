2348f54a1205ce1e967d086de4d052298afc41b0
salimi <salimiiz888@gmail.com>
2025-05-14 20:53:38 +0330

    Add setting to vite.config.js

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,5 +3,32 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';  
 
 export default defineConfig({  
-  plugins: [react(), tailwindcss()],  
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        // تقسیم‌بندی فایل‌ها و جدا کردن dependency ها
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // همه dependencyهای node_modules در فایل جدا قرار می‌گیرند
+          }
+        },
+        // نامگذاری فایل‌های پویا و قسمت‌های جدا شده
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        // تنظیم نام برای فایل‌های استاتیک مثل تصاویر و فونت‌ها
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg|mp4|webp)$/.test(name ?? '')) {
+            return 'assets/images/[name]-[hash][extname]';
+          }
+          if (/\.(ttf|woff2?|eot)$/.test(name ?? '')) {
+            return 'assets/fonts/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+    // افزایش هشدار در مورد فایل‌های بزرگ (در KB)
+    chunkSizeWarningLimit: 1000,
+  }
 });
