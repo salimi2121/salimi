60be37ae35f1fd05d4d60f99bb2f629971955f85
salimi <salimiiz888@gmail.com>
2025-05-15 11:05:29 +0330

    Change App.jsx

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';  
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import { DataProvider } from './DataContext'
 import IndexRestauran from './components/main/restaurant/IndexRestaurant'
 import IndexSushi from './components/main/sushi/IndexSushi';
@@ -27,44 +27,44 @@ import IndexMenu1 from './components/main/menu/menu-list-one/indexMenu1';
 
 export default function App() {
     const [count, setCount] = useState(0)
-    
+
 
     return (
-        <Router  basename="/">  
-        <DataProvider>  
-            {/* <Header /> */}
-            <Routes>  
-                <Route path="/" element={<IndexRestauran />} />  
-                <Route path="/IndexSushi" element={<IndexSushi />} />  
-                <Route path="/IndexBeef" element={<IndexBeef />} />  
-                <Route path="/IndexBurger" element={<IndexBurger />} />  
-                <Route path="/IndexPizza" element={<IndexPizza />} />
-                <Route path="/IndexMenu1" element={<IndexMenu1 />} />
-                <Route path="/IndexMenu2" element={<IndexMenu2 />} />
-                <Route path="/IndexMenuGrid" element={<IndexMenuGrid />} />
-                <Route path="/IndexMenuDetails" element={<IndexMenuDetails />} />
-                <Route path="/IndexAbout" element={<IndexAbout />} />
-                <Route path="/IndexHistory" element={<IndexHistory />} />
-                <Route path="/IndexChef" element={<IndexChef />} />
-                <Route path="/IndexGallery" element={<IndexGallery />} />
-                <Route path="/IndexFaqs" element={<IndexFaqs />} />
-                <Route path="/Index404" element={<Index404 />} />
-                <Route path="/IndexShop" element={<IndexShop />} />
-                <Route path="/IndexCart" element={<IndexCart />} />
-                <Route path="/IndexCheckout" element={<IndexCheckout />} />
-                <Route path="/IndexBloggrid" element={<IndexBloggrid />} />
-                <Route path="/IndexBlogstandard" element={<IndexBlogstandard />} />
-                <Route path="/IndexBlogdetails" element={<IndexBlogdetails />} />
-                <Route path="/IndexContact" element={<IndexContact />} />
-
-                
+        <BrowserRouter basename="/">
+            <DataProvider>
+                {/* <Header /> */}
+                <Routes>
+                    <Route path="/" element={<IndexRestauran />} />
+                    <Route path="/IndexSushi" element={<IndexSushi />} />
+                    <Route path="/IndexBeef" element={<IndexBeef />} />
+                    <Route path="/IndexBurger" element={<IndexBurger />} />
+                    <Route path="/IndexPizza" element={<IndexPizza />} />
+                    <Route path="/IndexMenu1" element={<IndexMenu1 />} />
+                    <Route path="/IndexMenu2" element={<IndexMenu2 />} />
+                    <Route path="/IndexMenuGrid" element={<IndexMenuGrid />} />
+                    <Route path="/IndexMenuDetails" element={<IndexMenuDetails />} />
+                    <Route path="/IndexAbout" element={<IndexAbout />} />
+                    <Route path="/IndexHistory" element={<IndexHistory />} />
+                    <Route path="/IndexChef" element={<IndexChef />} />
+                    <Route path="/IndexGallery" element={<IndexGallery />} />
+                    <Route path="/IndexFaqs" element={<IndexFaqs />} />
+                    <Route path="/Index404" element={<Index404 />} />
+                    <Route path="/IndexShop" element={<IndexShop />} />
+                    <Route path="/IndexCart" element={<IndexCart />} />
+                    <Route path="/IndexCheckout" element={<IndexCheckout />} />
+                    <Route path="/IndexBloggrid" element={<IndexBloggrid />} />
+                    <Route path="/IndexBlogstandard" element={<IndexBlogstandard />} />
+                    <Route path="/IndexBlogdetails" element={<IndexBlogdetails />} />
+                    <Route path="/IndexContact" element={<IndexContact />} />
 
 
 
 
-            </Routes>  
-        </DataProvider>  
-    </Router>  
+
+
+                </Routes>
+            </DataProvider>
+        </BrowserRouter>
 
     )
 }
