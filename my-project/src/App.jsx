26e02a381301edf24c4c7bc3f55f9bd445d2955b
salimi <salimiiz888@gmail.com>
2025-05-15 10:19:36 +0330

    Change vite.config.js

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -30,7 +30,7 @@ export default function App() {
     
 
     return (
-        <Router>  
+        <Router  basename="/">  
         <DataProvider>  
             {/* <Header /> */}
             <Routes>  
--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite';  
-import react from '@vitejs/plugin-react';  
-import tailwindcss from '@tailwindcss/vite';  
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({  
+export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // اضافه کردن این بخش جدید
+  base: './', // برای رفع مشکل مسیرهای نسبی در production
+  
   build: {
+    outDir: 'dist', // اطمینان از ساخت خروجی در dist
     rollupOptions: {
       output: {
-        // تقسیم‌بندی فایل‌ها و جدا کردن dependency ها
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor'; // همه dependencyهای node_modules در فایل جدا قرار می‌گیرند
+            return 'vendor';
           }
         },
-        // نامگذاری فایل‌های پویا و قسمت‌های جدا شده
         chunkFileNames: 'assets/js/[name]-[hash].js',
         entryFileNames: 'assets/js/[name]-[hash].js',
-        // تنظیم نام برای فایل‌های استاتیک مثل تصاویر و فونت‌ها
         assetFileNames: ({ name }) => {
           if (/\.(gif|jpe?g|png|svg|mp4|webp)$/.test(name ?? '')) {
             return 'assets/images/[name]-[hash][extname]';
@@ -28,7 +29,6 @@ export default defineConfig({
         },
       },
     },
-    // افزایش هشدار در مورد فایل‌های بزرگ (در KB)
     chunkSizeWarningLimit: 1000,
   }
 });
