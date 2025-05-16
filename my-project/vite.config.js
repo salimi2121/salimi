6150e37d0ab59136906620190abd939d1b07ec46
salimi <salimiiz888@gmail.com>
2025-05-16 19:54:32 +0330

    create static.json

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // اضافه کردن این بخش جدید
+  
   base: '/', // برای رفع مشکل مسیرهای نسبی در production
   
   build: {
